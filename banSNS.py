4e636d7036669bd18236acae9299ab06ae5f5300
ktakagiw <ktakagiw@cisco.com>
2014-01-05 14:37:31 -0800

    add twitter

--- banSNS.py
+++ banSNS.py
@@ -6,24 +6,50 @@ import subprocess
 import sys
  
 parser = argparse.ArgumentParser(description='Modify /private/etc/hosts to ban to access fb')
-parser.add_argument('-m','--mode', help='mode ban/allow',required=True)
+parser.add_argument('-s','--status', help='mode ban/allow', required=True)
 args = parser.parse_args()
  
 lines = []
 hosts_file_path = '/private/etc/hosts'
-mode = args.mode
+status = args.status
+facebooks = 'facebook.com www.facebook.com login.facebook.com'.split()
+twitters = 'twitter.com www.twitter.com'.split()
 
-with open(hosts_file_path, 'r') as f:
-	for line in f.readlines():
-		if mode == 'allow':
-			if line.find('facebook.com') != -1 and line[0] != '#':
-				line = "#" + line
-		elif mode == 'ban':
-			if line.find('facebook.com') != -1 and line[0] == '#':
-				line = line[1:]
-		else:
-			sys.exit(0)
-		lines.append(line)
+if not status in 'allow ban origin backup'.split():
+	sys.exit(0)
+
+if not status == 'origin':
+	with open(hosts_file_path, 'r') as f:
+		if status == 'allow':
+			for line in f.readlines():
+				for sns in facebooks + twitters:
+					if line.find(sns) != -1:
+						break
+				else:
+					lines.append(line)
+
+		elif status == 'ban':
+			for line in f.readlines():
+				lines.append(line)
+
+			for sns in facebooks + twitters:
+				line = '127.0.0.1 ' + sns + "\n"
+				lines.append(line)
+
+elif status == 'origin':
+	origin = '''
+##
+# Host Database
+#
+# localhost is used to configure the loopback interface
+# when the system is booting.  Do not change this entry.
+##
+127.0.0.1                  localhost
+255.255.255.255	broadcasthost
+::1                            localhost 
+fe80::1%lo0	        localhost
+'''
+	lines = origin
 
 with open( hosts_file_path + '_tmp', 'w') as new_host:
 	for line in lines:
